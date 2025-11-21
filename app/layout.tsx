e307802cbdd4e919c2bf102f98f776de22e4799b
Rachael Grocott <rachaelgrocott@gmail.com>
2025-11-21 12:06:02 +0000

    fixed typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-rt { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
